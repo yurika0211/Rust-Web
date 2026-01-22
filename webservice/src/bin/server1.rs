5da77a398d0d99a6272eb2028138d43fa7a0decc
shiokou <shiokou0408@gmail.com>
2026-01-22 10:27:58 +0800

    创建一个基于aticx的server1

---
+++ webservice/src/bin/server1.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+//配置route
+
+pub fn general_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/health", web::get().to(health_check_handler));
+}
+
+//配置handler
+pub async fn health_check_handler() -> impl Responder {
+    HttpResponse::Ok().json("Server is running...")
+}
+
+//实例化HTTP server并运行
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    //构建app,配置route
+    let app = move || App::new().configure(general_routes);
+
+    //运行HTTP server
+    HttpServer::new(app).bind("127.0.0.1:8080")?.run().await
+}
