01e6c77f36f1d00be033a88feb17737ca672e191
shiokou <shiokou0408@gmail.com>
2026-01-24 00:22:44 +0800

    构建了健康检查和上传课程

---
+++ webservice/src/handlers.rs
@@ -0,0 +1,64 @@
+use super::state::AppState;
+use actix_web::{HttpResponse, http::StatusCode, web};
+
+pub async fn health_check_handler (
+    app_state: web::Data<AppState>
+) -> HttpResponse {
+    let health_check_response = &app_state.health_check_response;
+    let mut visit_count = app_state.visit_count.lock().unwrap();
+    let response = 
+        format!("{}{} times", health_check_response, visit_count);
+    *visit_count += 1;
+    HttpResponse::Ok().json(&response)
+}
+
+use super::models::Course;
+use chrono::Utc;
+pub async fn new_course(
+    new_course: web::Json<Course>,
+    app_state: web::Data<AppState>
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.teacher_id == new_course.teacher_id)
+        .collect::<Vec<Course>>()
+        .len();
+    let new_course = Course {
+        teacher_id: new_course.teacher_id,
+        id: Some(course_count + 1),
+        name: new_course.name.clone(),
+        time: Some(Utc::now().naive_utc()),
+    };
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Course added successfully")
+}
+
+use actix_web;
+
+#[actix_rt::test]
+async fn post_course_test() {
+    let course = web::Json(
+        Course {
+            teacher_id: 1,
+            id: None,
+            name: "Test course".into(),
+            time: None,
+        }
+    );
+    use std::sync::Mutex;
+
+    let app_state: web::Data<AppState> = web::Data::new(AppState {
+        health_check_response: "".to_string(),
+        visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
+    });
+    let resp = new_course(course, app_state).await;
+    assert_eq!(resp.status(), StatusCode::OK);
+}
+
+
